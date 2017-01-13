bbf4c73b2ed91da859f59b4bbfe1b2031d524d7b
Matt Atkinson <matt.atkinson@imgtec.com>
2017-01-13 14:03:33 +0000

    Update to use awaclientd instead of static libraries
    Signed-off-by: Matt Atkinson <matt.atkinson@imgtec.com>

--- Switch/switch.c
+++ Switch/switch.c
@@ -1,116 +1,64 @@
 #include <signal.h>
-#include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-#include <time.h>
-#include <unistd.h>
+
+#include <awa/common.h>
+#include <awa/client.h>
+
 #include <letmecreate/letmecreate.h>
-#include <awa/static.h>
 
-#define COUNTER_INSTANCES       (1)
-#define OBJECT_INSTANCE_ID      (0)
+#define OPERATION_PERFORM_TIMEOUT   (1000)
 
+static int currentcount;
+static AwaClientSession * session;
 
-/**Define DeviceDetails Object**/
-typedef struct
-{
-    char Manufacturer[64];
-
-} DeviceDetailsObject;
-
-static DeviceDetailsObject device[1];
-
-static void DefineDeviceDetailsObject(AwaStaticClient * awaClient)
-{
-    AwaStaticClient_DefineObject(awaClient, 3, "DeviceDetails", 0, 1);
-    AwaStaticClient_DefineResource(awaClient, 3, 0, "Manufacturer", AwaResourceType_String, 0, 1, AwaResourceOperations_ReadOnly);
-    AwaStaticClient_SetResourceStorageWithPointer(awaClient, 3, 0, &device[0].Manufacturer, sizeof(device[0].Manufacturer), sizeof(device[0]));
-}
-
-/**Define SwitchCounter Object**/
-typedef struct
-{
-    int Totalcount;
-
-} SwitchCounterObject;
-
-static SwitchCounterObject counter[COUNTER_INSTANCES];
-
-static void DefineSwitchCounterObject(AwaStaticClient * awaClient)
-{
-    AwaStaticClient_DefineObject(awaClient, 3200, "Digital Input", 0, COUNTER_INSTANCES);
-    AwaStaticClient_DefineResource(awaClient, 3200, 5501, "Counter",  AwaResourceType_Integer, 0, 1, AwaResourceOperations_ReadOnly);
-    AwaStaticClient_SetResourceStorageWithPointer(awaClient, 3200, 5501, &counter[0].Totalcount, sizeof(counter[0].Totalcount), sizeof(counter[0]));
-}
-
-/**Give Resources a starting value**/
-static void SetInitialValues(AwaStaticClient * awaClient)
- {
-    AwaStaticClient_CreateObjectInstance(awaClient, 3200, OBJECT_INSTANCE_ID);
-    AwaStaticClient_CreateObjectInstance(awaClient, 3, OBJECT_INSTANCE_ID);
-
-    AwaStaticClient_CreateResource(awaClient, 3, OBJECT_INSTANCE_ID, 0);
-    strcpy(device[OBJECT_INSTANCE_ID].Manufacturer, "Creator Digital Input");
-
-    AwaStaticClient_CreateResource(awaClient, 3200, OBJECT_INSTANCE_ID, 5501);
-    counter[OBJECT_INSTANCE_ID].Totalcount = 0;
-}
-
-bool ReadCertificate(const char *filePath, uint8_t **certificate, uint32_t *certificate_length)
-{
-    FILE *inputFile = fopen(filePath, "rb");
-    if (inputFile == NULL)
-    {
-        printf("Unable to open certificate file under: %s", filePath);
-        return false;
-    }
-    if (fseek(inputFile, 0, SEEK_END) != 0)
-    {
-        fclose(inputFile);
-        printf("Can't set file offset.");
-        return false;
-    }
-    *certificate_length = ftell(inputFile);
-    rewind(inputFile);
-    *certificate = malloc(*certificate_length * (sizeof(uint8_t)));
-    if (fread(*certificate, sizeof(uint8_t), *certificate_length, inputFile) != *certificate_length)
-    {
-        printf("Could not read certificate file");
-        fclose(inputFile);
-        return false;
-    }
-    if (fclose(inputFile) == EOF)
-    {
-        printf("Couldn't close certificate file.");
-        return false;
-    }
-    return true;
-}
-
-static AwaStaticClient * awaClient = NULL;
-
-/**Switch Callback Function**/
-static int currentcount = 0;
-
-static void addcount(void)
-{
-    currentcount++;
-    counter[0].Totalcount = currentcount;
-    AwaStaticClient_ResourceChanged(awaClient, 3200, 0, 5501);
-}
-
+//Clean exit logic
 static volatile bool running = true;
 
-static void exit_program()
+static void exit_program(int __attribute__ ((unused))signo)
 {
     running = false;
 }
 
+/* Definition of the Digital Input IPSO object */
+static void DefineButtonObject(void)
+{
+    AwaObjectDefinition * objectDefinition = AwaObjectDefinition_New(3200, "Button", 0, 1);
+    AwaObjectDefinition_AddResourceDefinitionAsInteger(objectDefinition, 5501, "Count", false, AwaResourceOperations_ReadOnly, 0);
+
+    AwaClientDefineOperation * operation = AwaClientDefineOperation_New(session);
+    AwaClientDefineOperation_Add(operation, objectDefinition);
+    AwaClientDefineOperation_Perform(operation, OPERATION_PERFORM_TIMEOUT);
+    AwaClientDefineOperation_Free(&operation);
+}
+
+/* Create LWM2M Instances and Resources */
+static void SetInitialValues(void)
+{
+    AwaClientSetOperation * operation = AwaClientSetOperation_New(session);
+
+    AwaClientSetOperation_CreateObjectInstance(operation, "/3200/0");
+    AwaClientSetOperation_CreateOptionalResource(operation, "/3200/0/5501");
+
+    AwaClientSetOperation_Perform(operation, OPERATION_PERFORM_TIMEOUT);
+    AwaClientSetOperation_Free(&operation);
+}
+
+/* Counter callback function that is called on each button press */
+static void addcount()
+{
+    if (!session)
+        return;
+
+    AwaClientSetOperation * operation = AwaClientSetOperation_New(session);
+    currentcount++;
+    AwaClientSetOperation_AddValueAsInteger(operation, "/3200/0/5501", currentcount);
+    AwaClientSetOperation_Perform(operation, OPERATION_PERFORM_TIMEOUT);
+    AwaClientSetOperation_Free(&operation);
+}
+
 int main(void)
 {
-    char *clientName = "Creator Digital Input";
     /* Set signal handler to exit program when Ctrl+c is pressed */
     struct sigaction action = {
         .sa_handler = exit_program,
@@ -119,40 +67,26 @@ int main(void)
     sigemptyset(&action.sa_mask);
     sigaction (SIGINT, &action, NULL);
 
-    srand(time(NULL));
-    int port = 6000 + (rand() % 32768);
- 
-    uint32_t certificate_length = 0;
-    uint8_t *cert = NULL;
-    char *certFilePath = "/etc/config/creatorworkshop.crt"; // Path to Certificate file
+    /* Create and initialise client session */
+    session = AwaClientSession_New();
 
-    awaClient = AwaStaticClient_New();
+    /* Use default IPC configuration */
+    AwaClientSession_Connect(session);
 
-    AwaStaticClient_SetLogLevel(AwaLogLevel_Verbose);
-    AwaStaticClient_SetEndPointName(awaClient, clientName);
+    /* Set up LWM2M objects/instances/resources */
+    DefineButtonObject();
+    SetInitialValues();
 
-    ReadCertificate(certFilePath, &cert, &certificate_length);
-    AwaStaticClient_SetCertificate(awaClient, cert, certificate_length, AwaSecurityMode_Certificate);
-    
-    AwaStaticClient_SetCoAPListenAddressPort(awaClient, "0.0.0.0", port);
-    AwaStaticClient_SetBootstrapServerURI(awaClient, "coaps://deviceserver.creatordev.io:15684");
-
-    AwaStaticClient_Init(awaClient);
-
-    DefineSwitchCounterObject(awaClient);
-    DefineDeviceDetailsObject(awaClient);
-    SetInitialValues(awaClient);
-
-    /**Init Switch and Callback**/
+    /* Initialise Switch and Callback */
     switch_init();
     switch_add_callback(SWITCH_1_PRESSED, addcount);
 
     while (running)
-    {
-        AwaStaticClient_Process(awaClient);
-    }
+        AwaClientSession_Process(session, OPERATION_PERFORM_TIMEOUT);
 
-    AwaStaticClient_Free(&awaClient);
+    /* Cleanly close session */
+    AwaClientSession_Disconnect(session);
+    AwaClientSession_Free(&session);
 
     return 0;
 }
